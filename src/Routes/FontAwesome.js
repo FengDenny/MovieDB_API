6c21c5dd7507445f391552bed3e7cd3157767da8
FengDenny <fengdennyy@gmail.com>
2020-05-05 22:22:07 -0700

    Create FontAwesome.js

---
+++ src/Routes/FontAwesome.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
+library.add(faSpinner);
+
+
+// npm i --save @fortawesome/fontawesome-svg-core
+// npm i --save @fortawesome/free-solid-svg-icons
+// npm i --save @fortawesome/react-fontawesome
+
+
+function FontAwesome()
+{
+    return(
+        <FontAwesomeIcon icon="spinner" size="6x" spin /> 
+    )
+
+}
+
+export default FontAwesome
