3f11915124d78478c722f6ee8bdfdc67c052bcc5
FengDenny <fengdennyy@gmail.com>
2020-05-05 22:21:55 -0700

    movie/navbar route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Home from './Routes/Home'
+import Movie from './Routes/Movie'
+import NavBar from './NavBar/NavBar'
 
 
 function App() {
   return (
     <Router>
+      
     <div className="App">
+      <NavBar />
      <Route exact path="/" component={Home} />
+     <Route exact path="/movie/:movieID" component={Movie} />
     </div>
     </Router>
   );
